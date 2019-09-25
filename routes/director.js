81b0e85d1065f86df1c3c1250ef677eacf4febe8
aliosmanozoglu <a.osman.ozoglu@gmail.com>
2019-09-25 22:37:18 +0300

    director le movie sınıfı join işlemi ile ilişkilendirildi ve directorlerin altında çektikleri filmlerinde gösterilmesi sağlandı

--- routes/director.js
+++ routes/director.js
@@ -14,4 +14,50 @@ router.post('/', (req, res) => {
     });
 });
 
+router.get('/', (req, res) => {
+    Director.aggregate([
+        {
+            $lookup: {
+                from: 'movies',
+                foreignField: 'director_id',
+                localField: '_id',
+                as: 'movie'
+            }
+        },
+        {
+            $unwind: {          // yukarıda ki oluşturduğumuz movie verisinin diğer alanlarda da kullanılmasını sağlamak için yazıyoruz.
+                path: '$movie',
+                preserveNullAndEmptyArrays: true        // join işleminde join yapılmayan verilerinde görüntülenmesi için
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    _id: '$_id',            // burası grouplandırma yapıyor. yani aynı verilerin tek bir grup altında olmasını sağlıyor.
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                },
+                movies: {
+                    $push: '$movie'
+                }
+            }
+        },
+        {
+            $project: {                         // bizi iç içe gösterimden kurtarıyor. normalde id nin altında ayrı ayrı gösterilecek
+                _id: '$_id._id',                // olan veriler id olmadan göstermemizi sağlıyor.
+                name: '$_id.name',
+                surname: '$_id.surname',
+                bio: '$_id.bio',
+                movies: '$movies'
+            }
+        }
+    ])
+    .then(data => {
+        res.json(data);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
 module.exports = router;
