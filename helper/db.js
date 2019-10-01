e3acff540a73a90e0688e25ded4bebe8aa87ea71
aliosmanozoglu <a.osman.ozoglu@gmail.com>
2019-10-02 00:29:33 +0300

    edit

--- helper/db.js
+++ helper/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    mongoose.connect('mongodb+srv://movie_user:0321654987a.@movie-api-ippbg.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect('mongodb+srv://movie_user:0321654987a.@movie-api-ippbg.mongodb.net/movie-api?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
 
     mongoose.connection.on('open', () => {          // https://cloud.mongodb.com bağlantısı yapma
         console.log("MongoDB : Connected");
