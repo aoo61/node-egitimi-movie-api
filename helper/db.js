46fb46b7dd20860a306e13392211858a47ffb7ad
aliosmanozoglu <a.osman.ozoglu@gmail.com>
2019-10-01 21:59:26 +0300

    edit

--- helper/db.js
+++ helper/db.js
@@ -4,11 +4,11 @@ module.exports = () => {
     mongoose.connect('mongodb+srv://movie_user:0321654987a.@movie-api-ippbg.mongodb.net/movie-api?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
 
     mongoose.connection.on('open', () => {          // https://cloud.mongodb.com bağlantısı yapma
-        //console.log("MongoDB : Connected");
+        console.log("MongoDB : Connected");
     });
 
     mongoose.connection.on('error', (err) => {
-        console.log("MongoDB : Error ", err);
+        console.log("MongoDB : Error varrrrrrrrrrrrrrrr ", err);
     });
 
     mongoose.Promise = global.Promise;
